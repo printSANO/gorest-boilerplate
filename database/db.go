63091f7b4bee2b91e0af48ca98677ae15fac5fb4
Ryan <leehahnwoong@gmail.com>
2023-10-03 11:00:29 -0700

    feat: database connection function

--- database/db.go
+++ database/db.go
@@ -1,1 +1,27 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+)
+
+func ConnectDB(dbType string) (*sql.DB, error) {
+	urlDB := "postgres://postgres:postgres@localhost:5432/boilerplate"
+	// urlDB := os.Getenv("DB_URL")
+	db, err := sql.Open(dbType, urlDB)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/printSANO/gorest-boilerplate
 
 go 1.20
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
