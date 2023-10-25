21d9a32d01faa37528ca575aff561a5259c8fe72
Ryan <leehahnwoong@gmail.com>
2023-10-25 01:54:21 -0700

    ref: move database pointer to database package.
    Example codes for using database in handlers are given.

--- cmd/models/user.go
+++ cmd/models/user.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type Example struct {
-	ID        int       `db:"id" dataType:"integer" constraint:"PRIMARY KEY"`
+	ID        int       `db:"id" dataType:"serial" constraint:"PRIMARY KEY"`
 	Name      string    `db:"name" dataType:"varchar(20)"`
 	Password  string    `db:"password" dataType:"varchar(20)"`
 	CreatedAt time.Time `db:"created_at" dataType:"timestamp"`
 }
 
 type Example2 struct {
-	ID        int       `db:"id" dataType:"integer" constraint:"PRIMARY KEY"`
+	ID        int       `db:"id" dataType:"serial" constraint:"PRIMARY KEY"`
 	Title     string    `db:"title" dataType:"varchar(40)"`
 	Author    string    `db:"author" dataType:"varchar(20)"`
-	CreatedAt time.Time `db:"created_at" dataType:"timestamp"`
+	CreatedAt time.Time `db:"created_at" dataType:"timestamp DEFAULT CURRENT_TIMESTAMP"`
 }
